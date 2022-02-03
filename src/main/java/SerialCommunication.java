4ca524c51673e7b73f003b3fa99e971038c33334
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-03 08:36:34 +0100

    Playing around with serial communication.

---
+++ src/main/java/SerialCommunication.java
@@ -0,0 +1,34 @@
+import com.fazecast.jSerialComm.*;
+
+import java.util.Arrays;
+
+public class SerialCommunication {
+    public static void main(String[] args) {
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        if (!comPort.openPort()){
+            //TODO manage error
+        }
+
+        try {
+            boolean stop = false;
+            while (!stop) {
+                byte[] writeBuffer = new byte[]{0x06};
+                comPort.writeBytes(writeBuffer, writeBuffer.length);
+                if (comPort.bytesAvailable() == 0) {
+                    Thread.sleep(200);
+                    continue;
+                }
+
+                byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                int numRead = comPort.readBytes(readBuffer, comPort.bytesAvailable());
+                System.out.println("Read " + numRead + " bytes.");
+                System.out.println(Arrays.toString(Arrays.copyOfRange(readBuffer, 0, numRead)));
+                stop = true;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        comPort.closePort();
+    }
+}
