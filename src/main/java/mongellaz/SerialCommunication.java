ed6e6aae2f7cb67f093ed5cb38af4af52d4b2072
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-14 09:02:49 +0100

    Integrated Log4J and run 10 tests each time.

--- src/main/java/mongellaz/SerialCommunication.java
+++ src/main/java/mongellaz/SerialCommunication.java
@@ -1,14 +1,17 @@
 package mongellaz;
 
 import com.fazecast.jSerialComm.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 
 public class SerialCommunication {
     public static void main(String[] args) {
         SerialPort comPort = SerialPort.getCommPorts()[0];
+        Logger logger = LogManager.getLogger();
         if (!comPort.openPort()) {
-            System.err.println("Could not open serial port. Aborting");
+            logger.fatal("Could not open serial port. Aborting");
             return;
         }
 
@@ -16,34 +19,41 @@ public class SerialCommunication {
             // Wait for serial port to be ready
             Thread.sleep(3000);
 
-            // Write handshake
-            byte handshakeCode = 0x10;
-            byte[] softwareId = {0x01, (byte) 0xEE, 0x35, (byte) 0xD7, 0x2A, (byte) 0x80, 0x58, (byte) 0xEA};
-            byte endCode = 0x00;
-            byte[] writeBuffer = new byte[softwareId.length + 2];
-            writeBuffer[0] = handshakeCode;
-            System.arraycopy(softwareId, 0, writeBuffer, 1, softwareId.length);
-            writeBuffer[writeBuffer.length - 1] = endCode;
-            comPort.writeBytes(writeBuffer, writeBuffer.length);
+            int nChecks = 10;
+            for (int i = 0 ; i < nChecks ; i++) {
+                // Write handshake
+                byte handshakeCode = 0x10;
+                byte[] softwareId = {0x01, (byte) 0xEE, 0x35, (byte) 0xD7, 0x2A, (byte) 0x80, 0x58, (byte) 0xEA};
+                byte[] expectedResponse = {16, -123, -14, -98, -29, 67, 25, -22, -10};
+                byte endCode = 0x00;
+                byte[] writeBuffer = new byte[softwareId.length + 2];
+                writeBuffer[0] = handshakeCode;
+                System.arraycopy(softwareId, 0, writeBuffer, 1, softwareId.length);
+                writeBuffer[writeBuffer.length - 1] = endCode;
+                comPort.writeBytes(writeBuffer, writeBuffer.length);
 
-            // Read response
-            int responseSize = 9;
-            byte[] responseBuffer = new byte[responseSize];
-            int readBytes = 0;
-            while (readBytes < responseSize) {
-                int bufferSize = 128;
-                byte[] readBuffer = new byte[bufferSize];
-                int readBytesInOnce = comPort.readBytes(readBuffer, comPort.bytesAvailable());
-                if (readBytesInOnce > 0) {
-                    System.arraycopy(readBuffer, 0, responseBuffer, readBytes, readBytesInOnce);
-                    readBytes += readBytesInOnce;
-                    if (responseBuffer[readBytes - 1] == -1) {
-                        throw new ArduinoErrorException();
+                // Read response
+                int responseSize = 9;
+                byte[] responseBuffer = new byte[responseSize];
+                int readBytes = 0;
+                while (readBytes < responseSize) {
+                    int bufferSize = 128;
+                    byte[] readBuffer = new byte[bufferSize];
+                    int readBytesInOnce = comPort.readBytes(readBuffer, comPort.bytesAvailable());
+                    if (readBytesInOnce > 0) {
+                        System.arraycopy(readBuffer, 0, responseBuffer, readBytes, readBytesInOnce);
+                        readBytes += readBytesInOnce;
+                        if (responseBuffer[readBytes - 1] == -1) {
+                            throw new ArduinoErrorException();
+                        }
                     }
                 }
+                if (Arrays.equals(responseBuffer, expectedResponse)) {
+                    logger.info("OK");
+                } else {
+                    logger.error("Not OK");
+                }
             }
-
-            System.out.println(Arrays.toString(Arrays.copyOfRange(responseBuffer, 0, responseSize)));
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             e.printStackTrace();
