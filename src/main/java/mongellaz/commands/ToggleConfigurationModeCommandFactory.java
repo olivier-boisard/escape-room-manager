796eebbed160447184bd0c473cd949e3c3ed73a6
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-21 15:09:46 +0100

    Implemented Test for configuration mode.

---
+++ src/main/java/mongellaz/commands/ToggleConfigurationModeCommandFactory.java
@@ -0,0 +1,10 @@
+package mongellaz.commands;
+
+public class ToggleConfigurationModeCommandFactory implements ByteArrayFactory {
+    @Override
+    public byte[] generate() {
+        final byte lockCommand = 0x40;
+        final byte toggle = 0x03;
+        return new byte[]{lockCommand, toggle, END_OF_TRANSMISSION};
+    }
+}
