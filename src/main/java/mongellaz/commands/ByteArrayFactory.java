cabadaefa1fd21f5b4f86c69005c3063b8a7c9b0
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-21 08:18:31 +0100

    Declared endOfTransmission as static attribute of ByteArrayFactory.

--- src/main/java/mongellaz/commands/ByteArrayFactory.java
+++ src/main/java/mongellaz/commands/ByteArrayFactory.java
@@ -1,5 +1,8 @@
 package mongellaz.commands;
 
+@FunctionalInterface
 public interface ByteArrayFactory {
     byte[] generate();
+
+    byte END_OF_TRANSMISSION = 0x00;
 }
--- src/main/java/mongellaz/commands/HandshakeFactory.java
+++ src/main/java/mongellaz/commands/HandshakeFactory.java
@@ -6,13 +6,12 @@ public class HandshakeFactory implements ByteArrayFactory {
         // Variable declarations
         final byte handshakeCode = 0x10;
         final byte[] softwareId = {0x01, (byte) 0xEE, 0x35, (byte) 0xD7, 0x2A, (byte) 0x80, 0x58, (byte) 0xEA};
-        final byte endCode = 0x00;
 
         // Create byte array with data
         byte[] writeBuffer = new byte[softwareId.length + 2];
         writeBuffer[0] = handshakeCode;
         System.arraycopy(softwareId, 0, writeBuffer, 1, softwareId.length);
-        writeBuffer[writeBuffer.length - 1] = endCode;
+        writeBuffer[writeBuffer.length - 1] = END_OF_TRANSMISSION;
 
         // Return
         return writeBuffer;
