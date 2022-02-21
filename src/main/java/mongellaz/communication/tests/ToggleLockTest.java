af2fd78602ee2626a54f99ceddcebfc3f5bb417b
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-21 11:08:56 +0100

    Implemented Test for lock.

--- src/main/java/mongellaz/communication/tests/ToggleLockTest.java
+++ src/main/java/mongellaz/communication/tests/ToggleLockTest.java
@@ -1,6 +1,7 @@
 package mongellaz.communication.tests;
 
 import mongellaz.commands.ToggleLockCommandFactory;
+import mongellaz.communication.CommunicationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,13 +15,33 @@ public class ToggleLockTest {
     private static class ToggleLockResponseProcessor implements ResponseProcessor {
         @Override
         public void process(final byte[] response) {
+            processResponse(response);
+            waitOneSecond();
+        }
+
+        private void processResponse(byte[] response) {
             try {
-                final byte[] expectedResponse = {0x30, 0x03, 0};
-                if (Arrays.equals(response, expectedResponse)) {
-                    logger.info("OK");
+                final byte[] expectedResponse = {0x30, 0x03, (byte) 0xF1};
+                if (Arrays.equals(Arrays.copyOfRange(response, 0, expectedResponse.length), expectedResponse)) {
+                    final byte openStatus = 0x01;
+                    final byte closedStatus = 0x02;
+                    final byte status = response[3];
+                    String statusStr = switch (status) {
+                        case openStatus -> " - Open";
+                        case closedStatus -> " - Closed";
+                        default -> throw new CommunicationException("Unknown status " + status);
+                    };
+                    logger.info("OK - {}", statusStr);
                 } else {
                     logger.error("Not OK");
                 }
+            } catch (CommunicationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static void waitOneSecond() {
+            try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
