02e8c4e28f93b43eb3119df0354b6a395ee6fec2
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-23 15:28:34 +0100

    Refactored application.

--- src/main/java/mongellaz/application/Application.java
+++ src/main/java/mongellaz/application/Application.java
@@ -7,30 +7,42 @@ import org.apache.logging.log4j.Logger;
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        //Set up logger
         Logger logger = LogManager.getLogger();
+        logger.info("Application start");
+
+        // Set up basic resources handles
         SerialController controller = new SerialController();
         ResourcesCloser resourcesCloser = new ResourcesCloser();
         resourcesCloser.addCloseable(controller);
-        logger.info("Application start");
-        try {
-            controller.start();
-            JFrame frame = new JFrame("Ui");
 
+        try {
+            // Start controller
+            controller.start();
+
+            // Create UI
             Ui ui = new Ui(controller);
             controller.addLockStateObserver(ui);
 
+            // Set up UI
+            JFrame frame = new JFrame("Ui");
             frame.setContentPane(ui.getMainPanel());
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    super.windowClosing(e);
+                    resourcesCloser.closeResources();
+                }
+            });
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.pack();
+
+            // Start UI
             frame.setVisible(true);
-            frame.addWindowListener(resourcesCloser);
+
         } catch (CommunicationException e) {
             logger.fatal(e.getMessage());
             resourcesCloser.closeResources();
@@ -38,36 +50,8 @@ public class Application {
         // We shouldn't use the "finally" block here to call controller.stop() because that block would be reached
         // before the end of the application
 
+        // End of application
         logger.info("Application end");
     }
 
-    private static class ResourcesCloser extends WindowAdapter {
-
-        public ResourcesCloser() {
-            this.closeables = new LinkedList<>();
-        }
-
-        @Override
-        public void windowClosing(WindowEvent e) {
-            super.windowClosing(e);
-            closeResources();
-        }
-
-        public void closeResources() {
-            for (Closeable closeable : closeables) {
-                try {
-                    closeable.close();
-                } catch (IOException ex) {
-                    logger.fatal("Error while closing resource: {}", ex.getMessage());
-                }
-            }
-        }
-
-        public void addCloseable(Closeable closeable) {
-            closeables.add(closeable);
-        }
-
-        private final List<Closeable> closeables;
-        private final Logger logger = LogManager.getLogger();
-    }
 }
