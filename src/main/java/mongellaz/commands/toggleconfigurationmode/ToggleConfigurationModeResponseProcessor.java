19e3fc24b25ae0e34743ebc313a43d76d2a072e2
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-23 14:00:49 +0100

    Added magnet state listener.

--- src/main/java/mongellaz/commands/toggleconfigurationmode/ToggleConfigurationModeResponseProcessor.java
+++ src/main/java/mongellaz/commands/toggleconfigurationmode/ToggleConfigurationModeResponseProcessor.java
@@ -6,23 +6,34 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ToggleConfigurationModeResponseProcessor implements ResponseProcessor {
+
     @Override
     public void process(final byte[] response) {
         final byte commandCode = 0x40;
-        if (response[0]==commandCode) {
+        if (response[0] == commandCode) {
             try {
                 final byte[] expectedResponse = {commandCode, 0x03, (byte) 0xF1};
                 if (Arrays.equals(Arrays.copyOfRange(response, 0, expectedResponse.length), expectedResponse)) {
                     final byte enabledStatus = 0x03;
                     final byte disabledStatus = 0x04;
                     final byte status = response[3];
+                    MagnetState magnetState;
                     String statusStr = switch (status) {
-                        case enabledStatus -> " - Enabled";
-                        case disabledStatus -> " - Disabled";
+                        case enabledStatus -> {
+                            magnetState = MagnetState.OPEN;
+                            yield " - Enabled";
+                        }
+                        case disabledStatus -> {
+                            magnetState = MagnetState.CLOSED;
+                            yield " - Disabled";
+                        }
                         default -> throw new CommunicationException("Unknown status " + status);
                     };
+                    notifyAllMagnetStateObservers(magnetState);
                     logger.info("OK - {}", statusStr);
                 } else {
                     logger.error("Not OK");
@@ -35,5 +46,25 @@ public class ToggleConfigurationModeResponseProcessor implements ResponseProcess
         }
     }
 
+    public enum MagnetState {
+        OPEN,
+        CLOSED
+    }
+
+    public void addMagnetStateObserver(MagnetStateObserver magnetStateObserver) {
+        magnetStateObservers.add(magnetStateObserver);
+    }
+
+    public void notifyAllMagnetStateObservers(MagnetState newMagnetState) {
+        for (MagnetStateObserver magnetStateObserver : magnetStateObservers) {
+            magnetStateObserver.update(newMagnetState);
+        }
+    }
+
+    public interface MagnetStateObserver {
+        void update(MagnetState magnetState);
+    }
+
+    private final List<MagnetStateObserver> magnetStateObservers = new LinkedList<>();
     private final Logger logger = LogManager.getLogger();
 }
