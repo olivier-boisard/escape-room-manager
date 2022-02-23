4fdf4d6e2a79d59d9c4eb48bc001fba2b2a46042
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-23 20:37:41 +0100

    Made HandshakeResponseProcessor observable.

--- src/main/java/mongellaz/commands/handshake/HandshakeResponseProcessor.java
+++ src/main/java/mongellaz/commands/handshake/HandshakeResponseProcessor.java
@@ -1,10 +1,13 @@
 package mongellaz.commands.handshake;
 
+import mongellaz.commands.HandshakeResultObserver;
 import mongellaz.commands.ResponseProcessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class HandshakeResponseProcessor implements ResponseProcessor {
     @Override
@@ -12,17 +15,32 @@ public class HandshakeResponseProcessor implements ResponseProcessor {
         final byte commandCode = 0x10;
         if (response[0] == commandCode) {
             final byte[] expectedResponse = {commandCode, -123, -14, -98, -29, 67, 25, -22, -10};
+            HandshakeResult handshakeResult;
             if (Arrays.equals(response, expectedResponse)) {
+                handshakeResult = HandshakeResult.SUCCESS;
                 logger.info("Received expected response");
             } else {
+                handshakeResult = HandshakeResult.FAILURE;
                 if (logger.isErrorEnabled()) {
                     logger.error("Invalid response: {}", Arrays.toString(response));
                 }
             }
+            notifyAllHandshakeResultObserver(handshakeResult);
         } else {
             logger.debug("Ignoring command with code {}", commandCode);
         }
     }
 
+    public void addHandshakeResultObserver(HandshakeResultObserver handshakeResultObserver) {
+        handshakeResultObservers.add(handshakeResultObserver);
+    }
+
+    private void notifyAllHandshakeResultObserver(HandshakeResult handshakeResult) {
+        for (HandshakeResultObserver handshakeResultObserver : handshakeResultObservers) {
+            handshakeResultObserver.update(handshakeResult);
+        }
+    }
+
     private final Logger logger = LogManager.getLogger();
+    private final List<HandshakeResultObserver> handshakeResultObservers = new LinkedList<>();
 }
