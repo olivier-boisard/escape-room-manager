bbbb741cd3f620949eaeb064de57eb0d9e118656
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-24 12:54:09 +0100

    Moved class.

--- src/main/java/mongellaz/bookpuzzle/BookPuzzleDeviceStateObservable.java
+++ src/main/java/mongellaz/bookpuzzle/BookPuzzleDeviceStateObservable.java
@@ -1,11 +1,11 @@
-package mongellaz.application;
+package mongellaz.bookpuzzle;
 
 import mongellaz.commands.ConfigurationModeStateObserver;
 import mongellaz.commands.HandshakeResultObserver;
 import mongellaz.commands.LockStateObserver;
 import mongellaz.commands.PiccReaderStatusesObserver;
 
-public interface ArduinoBoardStateObservable {
+public interface BookPuzzleDeviceStateObservable {
     void addHandshakeResultObserver(HandshakeResultObserver handshakeResultObserver);
     void addLockStateObserver(LockStateObserver lockStateObserver);
     void addConfigurationModeStateObserver(ConfigurationModeStateObserver configurationModeStateObserver);
