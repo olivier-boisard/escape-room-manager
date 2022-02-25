d16e7be09ecee4d132025b9b6d06cc6e8e425f50
Olivier Boisard <olivier.boisard@gmail.com>
2022-02-25 11:35:03 +0100

    Refactored ToggleConfigurationModeResponseProcessor.

--- src/main/java/mongellaz/commands/toggleconfigurationmode/ToggleConfigurationModeResponseProcessor.java
+++ src/main/java/mongellaz/commands/toggleconfigurationmode/ToggleConfigurationModeResponseProcessor.java
@@ -1,7 +1,6 @@
 package mongellaz.commands.toggleconfigurationmode;
 
 import mongellaz.commands.ConfigurationModeStateObserver;
-import mongellaz.commands.ResponseProcessor;
 import mongellaz.communication.ByteArrayObserver;
 import mongellaz.communication.CommunicationException;
 import org.apache.logging.log4j.LogManager;
@@ -13,39 +12,47 @@ import java.util.List;
 
 public class ToggleConfigurationModeResponseProcessor implements ByteArrayObserver {
 
+    public static final byte COMMAND_CODE = 0x40;
+
     @Override
     public void update(final byte[] response) {
-        final byte commandCode = 0x40;
-        if (response[0] == commandCode) {
+        if (response[0] == COMMAND_CODE) {
             try {
-                final byte[] expectedResponse = {commandCode, 0x03, (byte) 0xF1};
-                if (Arrays.equals(Arrays.copyOfRange(response, 0, expectedResponse.length), expectedResponse)) {
-                    final byte enabledStatus = 0x03;
-                    final byte disabledStatus = 0x04;
-                    final byte status = response[3];
-                    ConfigurationModeState configurationModeState;
-                    String statusStr = switch (status) {
-                        case enabledStatus -> {
-                            configurationModeState = ConfigurationModeState.ENABLED;
-                            yield " - Enabled";
-                        }
-                        case disabledStatus -> {
-                            configurationModeState = ConfigurationModeState.DISABLED;
-                            yield " - Disabled";
-                        }
-                        default -> throw new CommunicationException("Unknown status " + status);
-                    };
-                    notifyAllConfigurationModeStateObservers(configurationModeState);
-                    logger.info("OK - {}", statusStr);
-                } else {
-                    logger.error("Not OK");
-                }
+                checkResponse(response);
+                runResponseIsValidProcess(response[3]);
             } catch (CommunicationException e) {
-                e.printStackTrace();
+                logger.error(e.getMessage());
             }
         } else {
-            logger.debug("Ignoring command with code {}", commandCode);
+            logger.debug("Ignoring command with code {}", COMMAND_CODE);
         }
+    }
+
+    private void checkResponse(byte[] response) throws CommunicationException {
+        final byte[] expectedResponse = {COMMAND_CODE, 0x03, (byte) 0xF1};
+        if (!Arrays.equals(Arrays.copyOfRange(response, 0, expectedResponse.length), expectedResponse)) {
+            throw new CommunicationException("Unexpected Response");
+        }
+    }
+
+    private void runResponseIsValidProcess(byte status) throws CommunicationException {
+        final byte enabledStatus = 0x03;
+        final byte disabledStatus = 0x04;
+        ConfigurationModeState configurationModeState;
+        String statusStr;
+        switch (status) {
+            case enabledStatus -> {
+                configurationModeState = ConfigurationModeState.ENABLED;
+                statusStr = " - Enabled";
+            }
+            case disabledStatus -> {
+                configurationModeState = ConfigurationModeState.DISABLED;
+                statusStr = " - Disabled";
+            }
+            default -> throw new CommunicationException("Unknown status " + status);
+        }
+        logger.info("OK - {}", statusStr);
+        notifyAllConfigurationModeStateObservers(configurationModeState);
     }
 
     public void addConfigurationModeStateObserver(ConfigurationModeStateObserver configurationModeStateObserver) {
