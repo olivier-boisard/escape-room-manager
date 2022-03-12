b0b3c410b23e0515a6c386e3efb27fd089601bf2
Olivier Boisard <olivier.boisard@gmail.com>
2022-03-12 13:12:44 +0100

    Delegated construction of GraphicalUserInterface to Application.

--- src/main/java/mongellaz/modules/Application.java
+++ src/main/java/mongellaz/modules/Application.java
@@ -3,10 +3,13 @@ package mongellaz.modules;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import java.awt.*;
+
 public class Application {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new BookPuzzleModule());
-        UserInterface userInterface = injector.getInstance(UserInterface.class);
+        Container bookPuzzleUiContainer = injector.getInstance(Container.class);
+        GraphicalUserInterface userInterface = new GraphicalUserInterface(bookPuzzleUiContainer, "Puzzle des livres");
         userInterface.start();
     }
 }
