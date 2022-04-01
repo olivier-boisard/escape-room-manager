c11c68bb17369f8e0c2e4302635fca4d6cddcc24
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-01 21:18:35 +0200

    Added UI for Wi-Fi configuration.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

---
+++ src/main/java/mongellaz/communication/serial/SerialPortWifiConfigurationUi.java
@@ -0,0 +1,12 @@
+package mongellaz.communication.serial;
+
+import javax.swing.*;
+
+@SuppressWarnings("unused")
+public class SerialPortWifiConfigurationUi {
+    private JLabel ssidLabel;
+    private JLabel passwordLabel;
+    private JTextField ssidTextField;
+    private JPasswordField passwordTextField;
+    private JButton wifiConfigurationButton;
+}
