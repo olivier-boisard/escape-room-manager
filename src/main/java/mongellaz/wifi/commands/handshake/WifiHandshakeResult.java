9d4a978c01b52c5de4df6976d15300cc3aa24bea
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-04 08:26:31 +0200

    Deleted unused classes.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/wifi/commands/handshake/WifiHandshakeResult.java
+++
@@ -1,6 +0,0 @@
-package mongellaz.wifi.commands.handshake;
-
-public enum WifiHandshakeResult {
-    SUCCESS,
-    FAILURE
-}
--- src/main/java/mongellaz/wifi/commands/handshake/WifiHandshakeResultObserver.java
+++
@@ -1,6 +0,0 @@
-package mongellaz.wifi.commands.handshake;
-
-@FunctionalInterface
-public interface WifiHandshakeResultObserver {
-    void update(WifiHandshakeResult wifiHandshakeResult);
-}
