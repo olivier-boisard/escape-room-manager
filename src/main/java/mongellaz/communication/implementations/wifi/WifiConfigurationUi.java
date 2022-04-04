ecb9b5cad935acbfb74cd7ff26bc2ce4263b67cf
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-04 12:42:13 +0200

    Added dialog box when Wi-Fi connection is established.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/communication/implementations/wifi/WifiConfigurationUi.java
+++ src/main/java/mongellaz/communication/implementations/wifi/WifiConfigurationUi.java
@@ -1,19 +1,28 @@
 package mongellaz.communication.implementations.wifi;
 
 import com.google.inject.Inject;
+import mongellaz.devices.wifi.commands.connection.ConnectionState;
+import mongellaz.devices.wifi.commands.connection.ConnectionStateObserver;
 import mongellaz.userinterface.ComponentHandler;
 
 import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("unused")
-public class WifiConfigurationUi implements ComponentHandler {
+public class WifiConfigurationUi implements ComponentHandler, ConnectionStateObserver {
 
     @Inject
     WifiConfigurationUi(WifiConfigurationObserver wifiConfigurationObserver) {
         wifiConfigurationButton.addActionListener(e -> wifiConfigurationObserver.update(
                 new WifiConfiguration(ssidTextField.getText(), passwordTextField.getPassword())
         ));
+    }
+
+    @Override
+    public void update(ConnectionState connectionState) {
+        int[] ipAddress = connectionState.ipAddress();
+        String ipAddressStr = ipAddress[0] + "." + ipAddress[1] + "." + ipAddress[2] + "." + ipAddress[3];
+        JOptionPane.showMessageDialog(mainPanel, "Addresse IP:" + ipAddressStr);
     }
 
     @Override
