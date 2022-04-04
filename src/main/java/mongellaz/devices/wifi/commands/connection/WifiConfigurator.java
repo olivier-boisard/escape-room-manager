093d2b5fdbb367e1d0dc9a7cdad585f2faa82a15
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-04 13:32:24 +0200

    Added message termination byte.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/devices/wifi/commands/connection/WifiConfigurator.java
+++ src/main/java/mongellaz/devices/wifi/commands/connection/WifiConfigurator.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import mongellaz.communication.manager.QueuedCommands;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @SuppressWarnings("ClassCanBeRecord")
 public class WifiConfigurator implements WifiConfigurationObserver {
@@ -26,17 +27,25 @@ public class WifiConfigurator implements WifiConfigurationObserver {
     }
 
     private byte[] extractSsid(WifiConfiguration wifiConfiguration) {
-        return wifiConfiguration.ssid().getBytes(StandardCharsets.US_ASCII);
+        byte[] ssid = wifiConfiguration.ssid().getBytes(StandardCharsets.US_ASCII);
+        byte[] outputBuffer = new byte[ssid.length + 1];
+        for (int i = 0; i < ssid.length; i++) {
+            outputBuffer[i] = ssid[0];
+        }
+        outputBuffer[outputBuffer.length - 1] = END_MESSAGE;
+        return outputBuffer;
     }
 
     private byte[] extractPassword(WifiConfiguration wifiConfiguration) {
         char[] password = wifiConfiguration.password();
-        byte[] passwordBytes = new byte[password.length];
+        byte[] passwordBytes = new byte[password.length + 1];
         for (int i = 0; i < password.length; i++) {
             passwordBytes[i] = (byte) password[i];
         }
+        passwordBytes[passwordBytes.length - 1] = END_MESSAGE;
         return passwordBytes;
     }
 
     private final QueuedCommands queuedCommands;
+    private static final byte END_MESSAGE = 0x00;
 }
