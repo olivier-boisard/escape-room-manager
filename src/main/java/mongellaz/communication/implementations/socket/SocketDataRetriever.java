73d920a806ca54506e32bb535d65ee7961d3c6d9
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-10 15:44:14 +0200

    Simplified SocketDataRetriever.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/communication/implementations/socket/SocketDataRetriever.java
+++ src/main/java/mongellaz/communication/implementations/socket/SocketDataRetriever.java
@@ -11,48 +11,50 @@ import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
 
-public class SocketDataRetriever implements Runnable {
+public class SocketDataRetriever {
 
     @Inject
-    SocketDataRetriever(Socket socket, List<ByteArrayObserver> byteArrayObservers) {
-        this.socket = socket;
+    SocketDataRetriever(List<ByteArrayObserver> byteArrayObservers) {
         this.byteArrayObservers = byteArrayObservers;
     }
 
-    public void run() {
-        try {
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            int bufferSize = 256;
-            final byte[] buffer = new byte[bufferSize];
-            int totalReadBytes = 0;
-            while (run) {
-                int readBytes = dataInputStream.read(buffer, totalReadBytes, buffer.length - totalReadBytes);
-                if (readBytes < 0) {
-                    logger.warn("Reached end of stream");
-                }
-                totalReadBytes += readBytes;
-                dispatchReadData(buffer, totalReadBytes);
+    public void loop() {
+        if (socket != null) {
+            try {
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                int bufferSize = 256;
+                final byte[] buffer = new byte[bufferSize];
+                int totalReadBytes = 0;
+                do {
+                    int readBytes = dataInputStream.read(buffer, totalReadBytes, buffer.length - totalReadBytes);
+                    if (readBytes < 0) {
+                        logger.warn("Reached end of stream");
+                    }
+                    totalReadBytes += readBytes;
+                    dispatchReadData(buffer, totalReadBytes);
+                } while (buffer[totalReadBytes - 1] != MESSAGE_END_CODE);
+            } catch (IOException e) {
+                logger.error("Could not get socket input stream: {}", e.getMessage());
             }
-        } catch (IOException e) {
-            logger.error("Could not get socket input stream: {}", e.getMessage());
         }
     }
 
-    public void stop() {
-        run = false;
+    public void setSocket(Socket socket) {
+        this.socket = socket;
     }
 
     private void dispatchReadData(byte[] buffer, int totalReadBytes) {
-        final byte messageEndCode = 0x00;
-        if (totalReadBytes > 0 && buffer[totalReadBytes - 1] == messageEndCode) {
+        logger.info("Dispatching socket message");
+        if (totalReadBytes > 0 && buffer[totalReadBytes - 1] == MESSAGE_END_CODE) {
             for (ByteArrayObserver byteArrayObserver : byteArrayObservers) {
                 byteArrayObserver.update(Arrays.copyOfRange(buffer, 0, totalReadBytes - 1));
             }
         }
     }
 
-    private final Socket socket;
+
+    private Socket socket;
     private final List<ByteArrayObserver> byteArrayObservers;
-    private volatile boolean run = true;
     private final Logger logger = LogManager.getLogger();
+    private static final byte MESSAGE_END_CODE = 0x00;
 }
