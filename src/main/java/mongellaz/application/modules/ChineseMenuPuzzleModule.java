ac2edec14b04134c59900253e9dbc3cf97c6c2ec
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-16 10:17:28 +0200

    Implemented handshake.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/application/modules/ChineseMenuPuzzleModule.java
+++ src/main/java/mongellaz/application/modules/ChineseMenuPuzzleModule.java
@@ -3,10 +3,12 @@ package mongellaz.application.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import mongellaz.communication.ByteArrayObserver;
 import mongellaz.communication.DeviceController;
+import mongellaz.communication.handshake.HandshakeResponseProcessor;
 import mongellaz.devices.chinesemenupuzzle.ChineseMenuPuzzleUi;
 import mongellaz.devices.chinesemenupuzzle.devicecontroller.ByteArrayControlledChineseMenuDeviceController;
 import mongellaz.devices.chinesemenupuzzle.devicecontroller.ChineseMenuDeviceController;
@@ -26,6 +28,10 @@ public class ChineseMenuPuzzleModule extends AbstractModule {
         bind(ComponentHandler.class).annotatedWith(Names.named("ControlUi")).to(ChineseMenuPuzzleUi.class);
         bind(DeviceController.class).to(ByteArrayControlledChineseMenuDeviceController.class);
         bind(ChineseMenuDeviceController.class).to(ByteArrayControlledChineseMenuDeviceController.class);
+        bind(ByteArrayObserver.class).annotatedWith(Names.named("HandshakeResponseProcessor")).to(HandshakeResponseProcessor.class);
+        bind(new TypeLiteral<byte[]>() {})
+                .annotatedWith(Names.named("ExpectedDeviceFirmwareId"))
+                .toInstance(new byte[]{0x30, 0x2B, 0x2A, 0x74, (byte) 0xCC, (byte) 0xAF, 0x59, (byte) 0x86});
     }
 
     @SuppressWarnings("unused")
@@ -42,7 +48,11 @@ public class ChineseMenuPuzzleModule extends AbstractModule {
 
     @SuppressWarnings("unused")
     @Provides
-    private static Iterable<ByteArrayObserver> provideByteArrayObservers() {
-        return new ArrayList<>(); //TODO
+    private static Iterable<ByteArrayObserver> provideByteArrayObservers(
+            @Named("HandshakeResponseProcessor") ByteArrayObserver handshakeResponseProcessor
+    ) {
+        ArrayList<ByteArrayObserver> byteArrayObservers = new ArrayList<>();
+        byteArrayObservers.add(handshakeResponseProcessor);
+        return byteArrayObservers;
     }
 }
