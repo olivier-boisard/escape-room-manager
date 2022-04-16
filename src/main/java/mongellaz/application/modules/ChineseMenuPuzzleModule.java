1b4429cb1c202247e3d68d5a34ac04e17f921948
Olivier Boisard <olivier.boisard@gmail.com>
2022-04-16 12:03:14 +0200

    Started implementation of ChineseMenuStatusRequestResponseProcessor.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/application/modules/ChineseMenuPuzzleModule.java
+++ src/main/java/mongellaz/application/modules/ChineseMenuPuzzleModule.java
@@ -10,6 +10,7 @@ import mongellaz.communication.ByteArrayObserver;
 import mongellaz.communication.DeviceController;
 import mongellaz.communication.handshake.HandshakeResponseProcessor;
 import mongellaz.devices.chinesemenupuzzle.ChineseMenuPuzzleUi;
+import mongellaz.devices.chinesemenupuzzle.commands.statusrequest.ChineseMenuStatusRequestResponseProcessor;
 import mongellaz.devices.chinesemenupuzzle.devicecontroller.ByteArrayControlledChineseMenuDeviceController;
 import mongellaz.devices.chinesemenupuzzle.devicecontroller.ChineseMenuDeviceController;
 import mongellaz.userinterface.ComponentHandler;
@@ -29,6 +30,7 @@ public class ChineseMenuPuzzleModule extends AbstractModule {
         bind(DeviceController.class).to(ByteArrayControlledChineseMenuDeviceController.class);
         bind(ChineseMenuDeviceController.class).to(ByteArrayControlledChineseMenuDeviceController.class);
         bind(ByteArrayObserver.class).annotatedWith(Names.named("HandshakeResponseProcessor")).to(HandshakeResponseProcessor.class);
+        bind(ByteArrayObserver.class).annotatedWith(Names.named("StatusRequestResponseProcessor")).to(ChineseMenuStatusRequestResponseProcessor.class);
         bind(new TypeLiteral<byte[]>() {})
                 .annotatedWith(Names.named("ExpectedDeviceFirmwareId"))
                 .toInstance(new byte[]{0x30, 0x2B, 0x2A, 0x74, (byte) 0xCC, (byte) 0xAF, 0x59, (byte) 0x86});
@@ -49,10 +51,12 @@ public class ChineseMenuPuzzleModule extends AbstractModule {
     @SuppressWarnings("unused")
     @Provides
     private static Iterable<ByteArrayObserver> provideByteArrayObservers(
-            @Named("HandshakeResponseProcessor") ByteArrayObserver handshakeResponseProcessor
+            @Named("HandshakeResponseProcessor") ByteArrayObserver handshakeResponseProcessor,
+            @Named("StatusRequestResponseProcessor") ByteArrayObserver statusRequestResponseProcessor
     ) {
         ArrayList<ByteArrayObserver> byteArrayObservers = new ArrayList<>();
         byteArrayObservers.add(handshakeResponseProcessor);
+        byteArrayObservers.add(statusRequestResponseProcessor);
         return byteArrayObservers;
     }
 }
---
+++ src/main/java/mongellaz/devices/chinesemenupuzzle/commands/statusrequest/ChineseMenuStatusRequestResponseProcessor.java
@@ -0,0 +1,82 @@
+package mongellaz.devices.chinesemenupuzzle.commands.statusrequest;
+
+import com.google.inject.Inject;
+import mongellaz.communication.ByteArrayObserver;
+import mongellaz.communication.CommunicationException;
+import mongellaz.devices.common.togglelock.LockState;
+import mongellaz.devices.common.togglelock.LockStateObserver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+public class ChineseMenuStatusRequestResponseProcessor implements ByteArrayObserver {
+    @Inject
+    ChineseMenuStatusRequestResponseProcessor(LockStateObserver lockStateObserver) {
+        this.lockStateObserver = lockStateObserver;
+    }
+
+    @Override
+    public void update(byte[] response) {
+        final byte expectedCommandCode = 0x20;
+        final byte currentWeightCode = 0x13;
+        final byte parametersCode = 0x23;
+        final byte lockStatusCode = 0x03;
+        final byte errorCode = (byte) 0xFF;
+        int index = 0;
+        if (response[0] == expectedCommandCode) {
+            try {
+                while (index < response.length) {
+                    final byte responseByte = response[index++];
+                    final byte[] unprocessedResponse = Arrays.copyOfRange(response, index, response.length);
+                    index += switch (responseByte) {
+                        case expectedCommandCode -> {
+                            logger.info("Command is 'status request'");
+                            yield 0;
+                        }
+                        case lockStatusCode -> processLockStatus(unprocessedResponse);
+                        case errorCode -> {
+                            logger.error("Received error code");
+                            yield 0;
+                        }
+                        default -> throw new CommunicationException("Unexpected byte: " + responseByte);
+                    };
+                }
+            } catch (CommunicationException e) {
+                logger.error(e.getMessage());
+            }
+        } else {
+            logger.debug("Ignoring command: {}", response);
+        }
+    }
+
+    private int processLockStatus(byte[] response) throws CommunicationException {
+        int index = 0;
+        final byte enabledCode = 0x03;
+        final byte disabledCode = 0x04;
+        byte lockStatus = response[index++];
+        LockState lockState;
+        String lockStatusMsg = "Lock is ";
+        switch (lockStatus) {
+            case enabledCode -> {
+                lockState = LockState.CLOSED;
+                lockStatusMsg += "locked";
+            }
+            case disabledCode -> {
+                lockState = LockState.OPEN;
+                lockStatusMsg += "unlocked";
+            }
+            default -> throw new CommunicationException("Unknown status " + lockStatus);
+        }
+        notifyLockStateObserver(lockState);
+        logger.info(lockStatusMsg);
+        return index;
+    }
+
+    private void notifyLockStateObserver(LockState lockState) {
+        lockStateObserver.update(lockState);
+    }
+
+    private final Logger logger = LogManager.getLogger();
+    private final LockStateObserver lockStateObserver;
+}
