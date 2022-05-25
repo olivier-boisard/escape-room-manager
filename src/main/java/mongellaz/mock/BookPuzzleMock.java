3e1267dd01c4994e5f8ff5d5d30562c14f4e9ff5
Olivier Boisard <olivier.boisard@gmail.com>
2022-05-25 08:27:47 +0200

    Extended mock.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/mock/BookPuzzleMock.java
+++ src/main/java/mongellaz/mock/BookPuzzleMock.java
@@ -22,8 +22,13 @@ public class BookPuzzleMock {
                     try (final DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
                         try (final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
                             byte[] inputBuffer = new byte[bufferSize];
-                            if (inputStream.read(inputBuffer) > 0 && inputBuffer[0] == 16) {
-                                outputStream.write(new byte[]{16, -123, -14, -98, -29, 67, 25, -22, -10});
+                            if (inputStream.read(inputBuffer) > 0) {
+                                logger.debug("Received bytes");
+                                if (inputBuffer[0] == 16){
+                                    outputStream.write(new byte[]{16, -123, -14, -98, -29, 67, 25, -22, -10, 0x00});
+                                } else if (inputBuffer[0] == 32) {
+                                    outputStream.write(new byte[]{32, 1, 5, 3, 3, 1, 3, 1, 2, 4, 3, 3, 0x00});
+                                }
                             }
                         }
                     }
