69753fe7874ef179a1b20a47b25301ba741a0a80
Olivier Boisard <olivier.boisard@gmail.com>
2022-05-25 09:00:19 +0200

    Fixed mock server.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/mock/BookPuzzleMock.java
+++ src/main/java/mongellaz/mock/BookPuzzleMock.java
@@ -3,9 +3,7 @@ package mongellaz.mock;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,17 +17,18 @@ public class BookPuzzleMock {
                 Socket socket = serverSocket.accept();
                 //noinspection InfiniteLoopStatement
                 while (true) {
-                    try (final DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
-                        try (final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
-                            byte[] inputBuffer = new byte[bufferSize];
-                            if (inputStream.read(inputBuffer) > 0) {
-                                logger.debug("Received bytes");
-                                if (inputBuffer[0] == 16){
-                                    outputStream.write(new byte[]{16, -123, -14, -98, -29, 67, 25, -22, -10, 0x00});
-                                } else if (inputBuffer[0] == 32) {
-                                    outputStream.write(new byte[]{32, 1, 5, 3, 3, 1, 3, 1, 2, 4, 3, 3, 0x00});
-                                }
-                            }
+                    final DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                    byte[] inputBuffer = new byte[bufferSize];
+                    int nReadBytes = inputStream.read(inputBuffer);
+                    if (nReadBytes > 0) {
+                        logger.debug("Received bytes");
+                        final DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+                        if (inputBuffer[0] == 16) {
+                            outputStream.write(new byte[]{16, -123, -14, -98, -29, 67, 25, -22, -10, 0x00});
+                            outputStream.flush();
+                        } else if (inputBuffer[0] == 32) {
+                            outputStream.write(new byte[]{32, 1, 5, 3, 3, 1, 3, 1, 2, 4, 3, 3, 0x00});
+                            outputStream.flush();
                         }
                     }
                 }
