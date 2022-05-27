29a168776418847a5c4026539c703bd2ec3ecf67
Olivier Boisard <olivier.boisard@gmail.com>
2022-05-27 08:18:53 +0200

    Send handshake and status request in puzzles.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/application/modules/ChineseMenuPuzzleSocketModule.java
+++ src/main/java/mongellaz/application/modules/ChineseMenuPuzzleSocketModule.java
@@ -3,6 +3,7 @@ package mongellaz.application.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import mongellaz.communication.ByteArrayGenerator;
+import mongellaz.devices.chinesemenupuzzle.commands.handshake.ChineseMenuPuzzleHandshakeFactory;
 import mongellaz.devices.chinesemenupuzzle.commands.statusrequest.ChineseMenuStatusRequestFactory;
 
 public class ChineseMenuPuzzleSocketModule extends AbstractModule {
@@ -10,6 +11,21 @@ public class ChineseMenuPuzzleSocketModule extends AbstractModule {
     protected void configure() {
         bind(ByteArrayGenerator.class)
                 .annotatedWith(Names.named("HeartBeatByteArrayGenerator"))
-                .to(ChineseMenuStatusRequestFactory.class);
+                .toInstance(new ByteArrayGenerator() {
+                    @Override
+                    public byte[] generate() {
+                        final byte[] handshake = handshakeFactory.generate();
+                        final byte[] statusRequest = statusRequestFactory.generate();
+                        int handshakeLength = handshake.length;
+                        int statusRequestLength = statusRequest.length;
+                        byte[] output = new byte[handshakeLength + statusRequestLength];
+                        System.arraycopy(handshake, 0, output, 0, handshakeLength);
+                        System.arraycopy(statusRequest, 0, output, handshakeLength, statusRequestLength);
+                        return output;
+                    }
+
+                    private final ChineseMenuPuzzleHandshakeFactory handshakeFactory = new ChineseMenuPuzzleHandshakeFactory();
+                    private final ChineseMenuStatusRequestFactory statusRequestFactory = new ChineseMenuStatusRequestFactory();
+                });
     }
 }
