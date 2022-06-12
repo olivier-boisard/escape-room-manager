aac1bceaff418d4f748591968d8331a68b2c651e
Olivier Boisard <olivier.boisard@gmail.com>
2022-06-12 08:33:58 +0200

    Send only status request in heartbeat.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/application/modules/BookPuzzleSocketModule.java
+++ src/main/java/mongellaz/application/modules/BookPuzzleSocketModule.java
@@ -3,7 +3,6 @@ package mongellaz.application.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import mongellaz.communication.ByteArrayGenerator;
-import mongellaz.devices.bookpuzzle.commands.handshake.BookPuzzleHandshakeFactory;
 import mongellaz.devices.bookpuzzle.commands.statusrequest.BookPuzzleStatusRequestFactory;
 
 public class BookPuzzleSocketModule extends AbstractModule {
@@ -11,21 +10,6 @@ public class BookPuzzleSocketModule extends AbstractModule {
     protected void configure() {
         bind(ByteArrayGenerator.class)
                 .annotatedWith(Names.named("HeartBeatByteArrayGenerator"))
-                .toInstance(new ByteArrayGenerator() {
-                    @Override
-                    public byte[] generate() {
-                        final byte[] handshake = handshakeFactory.generate();
-                        final byte[] statusRequest = statusRequestFactory.generate();
-                        int handshakeLength = handshake.length;
-                        int statusRequestLength = statusRequest.length;
-                        byte[] output = new byte[handshakeLength + statusRequestLength];
-                        System.arraycopy(handshake, 0, output, 0, handshakeLength);
-                        System.arraycopy(statusRequest, 0, output, handshakeLength, statusRequestLength);
-                        return output;
-                    }
-
-                    private final BookPuzzleHandshakeFactory handshakeFactory = new BookPuzzleHandshakeFactory();
-                    private final BookPuzzleStatusRequestFactory statusRequestFactory = new BookPuzzleStatusRequestFactory();
-                });
+                .to(BookPuzzleStatusRequestFactory.class);
     }
 }
