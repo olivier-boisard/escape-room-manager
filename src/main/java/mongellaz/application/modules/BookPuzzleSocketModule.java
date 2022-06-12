df2101798d4ac4a22fc15a2c15171ee604e535fa
Olivier Boisard <olivier.boisard@gmail.com>
2022-06-12 08:40:16 +0200

    Send only handshake in heartbeat.
    Signed-off-by: Olivier Boisard <olivier.boisard@gmail.com>

--- src/main/java/mongellaz/application/modules/BookPuzzleSocketModule.java
+++ src/main/java/mongellaz/application/modules/BookPuzzleSocketModule.java
@@ -3,13 +3,13 @@ package mongellaz.application.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import mongellaz.communication.ByteArrayGenerator;
-import mongellaz.devices.bookpuzzle.commands.statusrequest.BookPuzzleStatusRequestFactory;
+import mongellaz.devices.bookpuzzle.commands.handshake.BookPuzzleHandshakeFactory;
 
 public class BookPuzzleSocketModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ByteArrayGenerator.class)
                 .annotatedWith(Names.named("HeartBeatByteArrayGenerator"))
-                .to(BookPuzzleStatusRequestFactory.class);
+                .to(BookPuzzleHandshakeFactory.class);
     }
 }
--- src/main/java/mongellaz/application/modules/ChineseMenuPuzzleSocketModule.java
+++ src/main/java/mongellaz/application/modules/ChineseMenuPuzzleSocketModule.java
@@ -3,13 +3,13 @@ package mongellaz.application.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import mongellaz.communication.ByteArrayGenerator;
-import mongellaz.devices.chinesemenupuzzle.commands.statusrequest.ChineseMenuStatusRequestFactory;
+import mongellaz.devices.chinesemenupuzzle.commands.handshake.ChineseMenuPuzzleHandshakeFactory;
 
 public class ChineseMenuPuzzleSocketModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ByteArrayGenerator.class)
                 .annotatedWith(Names.named("HeartBeatByteArrayGenerator"))
-                .to(ChineseMenuStatusRequestFactory.class);
+                .to(ChineseMenuPuzzleHandshakeFactory.class);
     }
 }
